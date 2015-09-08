f2ead85ac4d55e2ea903f0a3f8839d30ea3c7ce4
Pallas <pallasravae@yahoo.com>
2015-09-08 12:40:05 -0600

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+for (var number = 1; number<=100;number++){
+	if ((number%3 === 0)&&(number%5 === 0))
+	{
+		console.log("fizzbuzz");
+	}
+	else if (number%5 === 0)
+	{
+		console.log("buzz");
+	}
+	else if (number%3 === 0)
+	{
+		console.log("fizz");
+	}
+	else
+	{
+		console.log(number);
+	}
+}
